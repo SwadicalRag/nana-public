fadacc344bf4bfefbee1d5f73dd5e56d715b9e03
Swad <SwadicalRag@users.noreply.github.com>
2016-02-23 18:33:59 +1030

    Ignore steam commands in a !help from discord

--- root/commands/help.lua
+++ root/commands/help.lua
@@ -1,14 +1,16 @@
 command.Add("help",function(argStr,reply,replyPersonal,user,chatroom)
     replyPersonal("== Nana commands: == \n")
     for commandName,commandData in pairs(command.commands) do
-        replyPersonal(
-            "    %s%s - %s%s (usage: %s)\n",
-            COMMAND_PREFIX,
-            commandName,
-            commandData.getRankFlag(),
-            commandData.description or "no description available",
-            commandData.usage or "call as is"
-        )
+        if not (commandData.handler and (commandData.handler == "steam") and not chatroom:IsSteam()) then
+            replyPersonal(
+                "    %s%s - %s%s (usage: %s)\n",
+                COMMAND_PREFIX,
+                commandName,
+                commandData.getRankFlag(),
+                commandData.description or "no description available",
+                commandData.usage or "call as is"
+            )
+        end
     end
     replyPersonal("== END ==\n")
     if chatroom and chatroom:IsSteam() then
