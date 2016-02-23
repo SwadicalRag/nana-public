ac5250c3059e99b82ce6483c5b7456e58286bdbc
Swad <SwadicalRag@users.noreply.github.com>
2016-02-23 15:06:04 +1030

    Async-ify

--- root/greeting.lua
+++ root/greeting.lua
@@ -1,12 +1,14 @@
 hook.Add("discord.userStatusChanged","greet",function(id,oldStatus,newStatus)
-    if not CookieLib:GetInternalPrivate(id).greeted then
-        if newStatus == "online" then
-            discordChannel.GetByName("general"):Say(string.format(
-                "Hello %s! Enjoy your stay.\nI am a lua bot connected to the steam group chat.",
-                discordUser.GetByID(id):Nick()
-            ))
-            CookieLib:GetInternalPrivate(id).greeted = true
-            CookieLib:Save()
+    timer.Simple(1,function()
+        if not CookieLib:GetInternalPrivate(id).greeted then
+            if newStatus == "online" then
+                discordChannel.GetByName("general"):Say(string.format(
+                    "Hello %s! Enjoy your stay.\nI am a lua bot connected to the steam group chat.",
+                    discordUser.GetByID(id):Nick()
+                ))
+                CookieLib:GetInternalPrivate(id).greeted = true
+                CookieLib:Save()
+            end
         end
-    end
+    end)
 end)
