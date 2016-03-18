15e1a27922d55b4c958d3983f75a8aac8b6497a5
SwadicalRag <SwadicalRag@users.noreply.github.com>
2016-03-19 04:25:10 +1030

    Update restart.lua

--- root/commands/restart.lua
+++ root/commands/restart.lua
@@ -12,3 +12,13 @@ command.Add("update",function(_,reply)
     reply("Telling nana-kernel to update and restart...")
     updateInternal();
 end,"Restarts nana",nil,COMMAND_MODERATOR)
+
+command.Add("core",function(msg,reply)
+    if msg == "true" then
+        reply("Core access enabled")
+        AllowAccessToCore = true
+    else
+        reply("Core access disabled")
+        AllowAccessToCore = false
+    end
+end,"Enables or disables core access",nil,COMMAND_ADMIN)
