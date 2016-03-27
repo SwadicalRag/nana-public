69931b04c9e1f5d8ae8b03def0f0f851a9222118
Swad <SwadicalRag@users.noreply.github.com>
2016-03-28 02:04:49 +1030

    aah

--- sandbox/tetris.lua
+++ sandbox/tetris.lua
@@ -124,10 +124,19 @@ function Tetris:CollisionTestActiveBlock(only_other_blocks)
         end
     end
 
+    local hit = false
+
+    self:IteratePixels(self.ActiveBlock.block,self.ActiveBlock.x,self.ActiveBlock.y,function(x,y)
+        if self.screenData[x] and self.screenData[x][y] then
+            hit = true
+            return true
+        end
+    end)
+
+    if hit then return true end
+
     if only_other_blocks then return false end
     if not self.Screen then return false end
-
-    local hit = false
 
     self:IteratePixels(self.ActiveBlock.block,self.ActiveBlock.x,self.ActiveBlock.y,function(x,y)
         if (y > self.Screen.h) or (x > self.Screen.w) then
@@ -143,28 +152,12 @@ function Tetris:Tick(screen,render,spawn_block_anyway)
     if self.GameOver then return self:DrawBoard(screen) end
 
     if self.ActiveBlock then
-        for i=1,#self.blockData do
-            if (self.blockData[i] ~= self.ActiveBlock) and self:CheckCollision(self.ActiveBlock,self.blockData[i],0,1) then
-                PrintInternal("HIT ACTIVE BLOCK")
-                self:CommitBlockToScreen(self.ActiveBlock)
-                self.ActiveBlock = nil
-                break
-            end
-        end
-
-        if self.ActiveBlock then
-            self.ActiveBlock.y = self.ActiveBlock.y + 1
-
-            self:IteratePixels(self.ActiveBlock.block,self.ActiveBlock.x,self.ActiveBlock.y,function(x,y)
-                if y > screen.h then
-                    self.ActiveBlock.y = self.ActiveBlock.y - 1
-                    self:CommitBlockToScreen(self.ActiveBlock)
-                    self.ActiveBlock = nil
-                    self.blockData[#self.blockData] = nil
-
-                    return true
-                end
-            end)
+        self.ActiveBlock.y = self.ActiveBlock.y + 1
+        if self:CollisionTestActiveBlock() then
+            self.ActiveBlock.y = self.ActiveBlock.y - 1
+            self:CommitBlockToScreen(self.ActiveBlock)
+            self.ActiveBlock = nil
+            self.blockData[#self.blockData] = nil
         end
     elseif render or spawn_block_anyway then
         -- no new blocks while 'skipping' frames
@@ -209,11 +202,60 @@ function Tetris:Tick(screen,render,spawn_block_anyway)
     end
 
     if render then
+        self:DrawScreen()
         self:DrawBoard(screen)
     end
 end
 
-Tetris:Reset()
+local chan_id = "162428115778404352"
+hook.Add("ChatMessage","Tetris",function(chatroom,user,_msg)
+    if (chatroom.id == chan_id) and Tetris.ActiveBlock and Tetris.Screen then
+        Tetris.lastPlayedNick = user:Nick() or "No one"
+        for i=1,#_msg do
+            msg = _msg:sub(i,i):lower()
+
+            if msg == "d" then
+                local orig_x = Tetris.ActiveBlock.x
+
+                local w,h = Tetris:BlockSize(Tetris.ActiveBlock.block)
+                Tetris.ActiveBlock.x = math.min(Tetris.ActiveBlock.x + 1,Tetris.Screen.w - w)
+
+                if Tetris:CollisionTestActiveBlock() then
+                    Tetris.ActiveBlock.x = orig_x
+                end
+            elseif msg == "a" then
+                local orig_x = Tetris.ActiveBlock.x
+
+                Tetris.ActiveBlock.x = math.max(1,Tetris.ActiveBlock.x - 1)
+
+                if Tetris:CollisionTestActiveBlock() then
+                    Tetris.ActiveBlock.x = orig_x
+                end
+            elseif msg == "w" then
+                local blockType,ang = Tetris.ActiveBlock.block:match("^(.-)(%d+)$")
+
+                ang = tonumber(ang) + 90
+                if ang == 360 then ang = 0 end
+
+                Tetris.ActiveBlock.block = blockType..ang
+
+                if Tetris:CollisionTestActiveBlock() then
+                    ang = ang - 90
+                    if ang == -90 then ang = 270 end
+                    Tetris.ActiveBlock.block = blockType..ang
+                end
+            elseif msg == "s" then
+                Tetris:Tick(Tetris.Screen,false)
+            elseif msg == "x" then
+                Tetris:Tick(Tetris.Screen,false,true)
+            elseif msg == "c" then
+                Tetris:Reset()
+            end
+
+            ::next_one::
+        end
+    end
+end)
 
 Tetris:RegisterBlock("L-1-0",{
     {X,O,O},
@@ -367,53 +409,7 @@ Tetris:RegisterBlock("Z-2-270",{
     {X,X,O}
 })
 
-local chan_id = "162428115778404352"
-hook.Add("ChatMessage","Tetris",function(chatroom,user,_msg)
-    if (chatroom.id == chan_id) and Tetris.ActiveBlock and Tetris.Screen then
-        Tetris.lastPlayedNick = user:Nick() or "No one"
-        for i=1,#_msg do
-            msg = _msg:sub(i,i):lower()
-
-            if msg == "d" then
-                for i=1,#Tetris.blockData do
-                    if (Tetris.blockData[i] ~= Tetris.ActiveBlock) and Tetris:CheckCollision(Tetris.ActiveBlock,Tetris.blockData[i],1,0) then
-                        goto next_one
-                    end
-                end
-
-                local w,h = Tetris:BlockSize(Tetris.ActiveBlock.block)
-                Tetris.ActiveBlock.x = math.min(Tetris.ActiveBlock.x + 1,Tetris.Screen.w - w)
-            elseif msg == "a" then
-                for i=1,#Tetris.blockData do
-                    if (Tetris.blockData[i] ~= Tetris.ActiveBlock) and Tetris:CheckCollision(Tetris.ActiveBlock,Tetris.blockData[i],1,0) then
-                        goto next_one
-                    end
-                end
-
-                Tetris.ActiveBlock.x = math.max(1,Tetris.ActiveBlock.x - 1)
-            elseif msg == "w" then
-                local blockType,ang = Tetris.ActiveBlock.block:match("^(.-)(%d+)$")
-
-                ang = tonumber(ang) + 90
-                if ang == 360 then ang = 0 end
-
-                Tetris.ActiveBlock.block = blockType..ang
-
-                if Tetris:CollisionTestActiveBlock() then
-                    ang = ang - 90
-                    if ang == -90 then ang = 270 end
-                    Tetris.ActiveBlock.block = blockType..ang
-                end
-            elseif msg == "s" then
-                Tetris:Tick(Tetris.Screen,false)
-            elseif msg == "x" then
-                Tetris:Tick(Tetris.Screen,false,true)
-            end
-
-            ::next_one::
-        end
-    end
-end)
+Tetris:Reset()
 
 hook.Add("Render","Tetris",function(screen)
     PrintInternal("TICK")
